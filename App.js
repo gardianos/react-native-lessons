ab933252ae8eca827a5002620cb2e3bec9776b60
gardianos <gardianii@hotmail.com>
2022-03-08 18:15:03 +0100

    component screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screen/HomeScreen";
+import ComponentScreen from "./src/screen/ComponentScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Component: ComponentScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Component",
     defaultNavigationOptions: {
       title: "App",
     },
